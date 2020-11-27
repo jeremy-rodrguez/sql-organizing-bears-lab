77a590f0bdf4b95c12be90e90b88c3add3c9a603
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2020-11-26 20:51:40 -0800

    Bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Mild', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Black','Kind', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Red', 'Cuddly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'F', 'Pink', 'Kind', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Blue', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Yellow', 'Nervous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Orange', 'Loud', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, 'M', 'Grizzly', 'Hungry', 0);
